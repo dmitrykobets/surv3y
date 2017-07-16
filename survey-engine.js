6f794cee3d5f36a1f95ac730aa59bc2c46f73180
dmitrykobets <dmitry.kobets212@gmail.com>
2017-07-16 10:36:07 -0400

    Added div structure support for the survey 'JSON'

--- survey-engine.js
+++ survey-engine.js
@@ -1,5 +1,24 @@
 variables = {}
 
+class Div {
+	constructor({classes=[], children=[]}) {
+		this.classes = classes;
+		this.id = Div.count;
+		Div.count ++;
+		this.selector = "#" + this.id;
+
+		this.children = children;
+	}
+}
+Div.count = 0;
+Div.prototype.render = function() {
+	var HTML = "";
+	this.children.forEach((e) => {
+		HTML += e.render();
+	})
+	return HTML
+}
+
 class SingularError {
 	constructor({name, message, visibleIf, appearOnChange=true}) {
 		this.name = name;
@@ -140,7 +159,7 @@ class Question {
 				this.visibilityDependants = [];
 				this.disabilityDependants = [];
 				this.linkToDependantQuestions = function(page) {
-					page.questions.forEach((q) => {
+					page.questions().forEach((q) => {
 						if (q.visibleIf.toString().includes('variables["' + this.name + '"]')) {
 							this.visibilityDependants.push(q.name);
 						} 
@@ -152,7 +171,7 @@ class Question {
 						$(this.inputSelector).keydown(debounce(
 							() => {
 								this.visibilityDependants.forEach((d) => {
-									page.questions.find((q) => {return q.name === d}).setVisibility(page);
+									page.questions().find((q) => {return q.name === d}).setVisibility(page);
 								})
 							}, 250
 						));
@@ -161,7 +180,7 @@ class Question {
 						$(this.inputSelector).keydown(debounce(
 							() => {
 								this.disabilityDependants.forEach((d) => {
-									page.questions.find((q) => {return q.name === d}).setDisability();
+									page.questions().find((q) => {return q.name === d}).setDisability();
 								})
 							}, 250
 						));
@@ -169,7 +188,7 @@ class Question {
 				}
 				this.errors = [];
 				this.linkToErrors = function(page) {
-					page.errors.forEach((e) => {
+					page.errors().forEach((e) => {
 						if (e.visibleIf.toString().includes('variables["' + this.name + '"]')) {
 							this.errors.push(e);
 						}
@@ -197,9 +216,6 @@ Question.prototype.render = function() {
 
 	var HTML = this.generateSkeletonHTML();
 	return HTML
-	if (append === true) {
-		containerJQuery.append(HTML);	
-	}	
 }
 Question.prototype.setProperties = function() {
 	this.setPlaceholder && this.setPlaceholder();
@@ -211,57 +227,95 @@ Question.prototype.setProperties = function() {
 class Page {
 	constructor({elements=[], visibleIf=true}) {
 		this.errorsInitialized = false;
-		this.questions = [];
-		this.errors = [];
 		this.elements = elements;
 		var templateSingularsToConvertIndices = [];
-		var i = 0;
-		while (i < this.elements.length) {
-			const elm = this.elements[i];
-			if (elm.constructor.name === "Question") {
-				/* -- high priority template registration */
-				if (elm.isRequired === true) {
-					this.elements.splice(i, 0, new SingularTemplateError ({
-						templateName: "required",
-						questionName: elm.name,
-					}));
-					templateSingularsToConvertIndices.push({elmIdx: i, errIdx: this.errors.length});
+
+		this.questions = () => {
+			const recurse = function(collection) {
+				var results = [];
+				for (var i in collection) {
+					if (collection[i].constructor.name === "Question") {
+						results.push(collection[i]);
+					} else if (collection[i].constructor.name === "Div") {
+						results = results.concat(recurse(collection[i].children));
+					}
 				}
-				this.questions.push(elm);
-				i ++;
-			} else if (elm.constructor.name === "SingularError") {
-				this.errors.push(elm);
-			} else if (elm.constructor.name === "SingularTemplateError") {
-				templateSingularsToConvertIndices.push({elmIdx: i, errIdx: this.errors.length});
+				return results;
 			}
-			i ++;
+			return recurse(this.elements);
+		}
+		this.errors = () => {
+			const recurse = function(collection) {
+				var results = [];
+				for (var i in collection) {
+					if (collection[i].constructor.name === "SingularError") {
+						results.push(collection[i]);
+					} else if (collection[i].constructor.name === "Div") {
+						results = results.concat(recurse(collection[i].children));
+					}
+				}
+				return results;
+			}
+			return recurse(this.elements);
 		}
 
-		this.initializeErrors = function(templates) {
-			templateSingularsToConvertIndices.forEach((indices) => {
-				const templateSingular = this.elements[indices.elmIdx];
-				const template = templates.find((temp) => {return temp.name === templateSingular.templateName});
-				const linkedQuestion = this.findQuestionByName(templateSingular.questionName);
+		this.initializeErrors = (templates) => {
+			const qs = this.questions();
+			const recurse = (collection) => {
+				var i = 0;
+				while (i < collection.length) {
+					// find which to convert
+					var singularTemplate = undefined;
+					var insertBefore;
+					if (collection[i].constructor.name === "SingularTemplateError") {
+						singularTemplate = collection[i];
+						insertBefore = false;
+					} else if (collection[i].constructor.name === "Question") {
+						/* -- high priority template registration */
+						if (collection[i].isRequired === true) {
+							singularTemplate = new SingularTemplateError({
+								templateName: "required",
+								questionName: collection[i].name,
+							})
+							insertBefore = true;
+						}
+					}
+					if (singularTemplate !== undefined) {
+						// convert
+						const template = templates.find((temp) => {return temp.name === singularTemplate.templateName});
+						const linkedQuestion = this.findQuestionByName(singularTemplate.questionName, qs);
 
-				var visibleIfFuncString = template.visibleIfFuncStrings[linkedQuestion.type];
-				visibleIfFuncString = visibleIfFuncString.replace("variables[]", 'variables["' + linkedQuestion.name + '"]');
+						var visibleIfFuncString = template.visibleIfFuncStrings[linkedQuestion.type];
+						visibleIfFuncString = visibleIfFuncString.replace("variables[]", 'variables["' + linkedQuestion.name + '"]');
 
-				this.errors.splice(indices.errIdx, 0, new SingularError({
-					name: templateSingular.templateName + templateSingular.questionName,
-					message: template.message,
-					visibleIf: eval("(" + visibleIfFuncString + ")"),
-					appearOnChange: template.appearOnChange,
-				}))
-				this.elements[indices.elmIdx] = this.errors[indices.errIdx]
-			})
+						const singularError = new SingularError({
+							name: singularTemplate.templateName + singularTemplate.questionName,
+							message: template.message,
+							visibleIf: eval("(" + visibleIfFuncString + ")"),
+							appearOnChange: template.appearOnChange,
+						})
+
+						if (insertBefore === true) {
+							collection.splice(i, 0, singularError);
+							i ++;
+						} else {
+							collection[i] = singularError;
+						}
+					} else if (collection[i].constructor.name === "Div") {
+						recurse(collection[i].children);
+					}
+					i ++;
+				}
+			}
+			recurse(this.elements);
 
 			// push questions each error depends on in terms of state (visible/disabled)
 			const varRegex = /variables\["(.+?)"\]/g;
-			this.errors.forEach((e) => {
+			this.errors().forEach((e) => {
 				const fString = e.visibleIf.toString();	
 				var match = varRegex.exec(fString);
 				while (match != null) {
-					e.questions.push(this.findQuestionByName(match[1]));
+					e.questions.push(this.findQuestionByName(match[1], qs));
 					match = varRegex.exec(fString);
 				}
 			})
@@ -281,24 +335,23 @@ Page.prototype.render = function(containerJQuery) {
 		$("#page").append(elm.render());
 	})
 
-
 	// pass 2: set question properties
-	this.questions.forEach((q) => {
+	this.questions().forEach((q) => {
 		q.setProperties();
 		q.linkToVariable && q.linkToVariable();
 		q.linkToDependantQuestions && q.linkToDependantQuestions(this);
 		q.linkToErrors && q.linkToErrors(this);
 	})
 
-	this.errors.forEach((e) => {
+	this.errors().forEach((e) => {
 		e.setVisibility(false);
 	})
 }
 Page.prototype.makeQuestionVisible = function(question) {
 	var prevElm = undefined;
 	for (var i = this.findQuestionIndex(question) - 1; i >= 0; i --) {
-		if (this.questions[i].visibleIf()) {
-			prevElm = $(this.questions[i].inputSelector);
+		if (this.questions()[i].visibleIf()) {
+			prevElm = $(this.questions()[i].inputSelector);
 			prevElm.after(question.render());
 			break;
 		}
@@ -313,10 +366,14 @@ Page.prototype.makeQuestionVisible = function(question) {
 	question.linkToErrors && question.linkToErrors(this);
 }
 Page.prototype.findQuestionIndex = function(question) {
-	return this.questions.indexOf(question);
+	return this.questions().indexOf(question);
 }
-Page.prototype.findQuestionByName = function(qName) {
-	return this.questions.find((q) => {return q.name === qName});
+Page.prototype.findQuestionByName = function(qName, preloaded=undefined) {
+	if (preloaded !== undefined) {
+		return preloaded.find((q) => {return q.name === qName});
+	} else {
+		return this.questions().find((q) => {return q.name === qName});
+	}
 }
 
 class Survey {
@@ -383,11 +440,11 @@ const surveyJSON = {
 	pages: [
 		new Page({
 			elements: [
-				/*
+				
 				new SingularTemplateError ({
 					templateName: "required",
-					questionName: "hi",
-				}),*/
+					questionName: "d",
+				}),
 				new Question({
 					type: Question.Types.TEXT,
 					placeholder: "hi",
@@ -396,19 +453,28 @@ const surveyJSON = {
 					name: "hi",
 					isRequired: true,
 				}),
-				new SingularError({
-					name: "e1",
-					message: "error",
-					visibleIf: function() {return variables["d"] === ""},
-				}),
-				new Question({
-					type: Question.Types.TEXT,
-					placeholder: "plasework",
-					defaultValue: "",
-					visibleIf: function() {return variables["hi"] !== "1"},
-					disabledIf: function() {return variables["hi"] !== "2"},
+				new Div({
+					children: [
+						/*
+						new SingularError({
+							name: "e1",
+							message: "expect to see me",
+							visibleIf: function() {return variables["d"] === ""},
+						})*/
+						new Div({
+							children: [
+								new Question({
+									type: Question.Types.TEXT,
+									placeholder: "plasework",
+									defaultValue: "",
+									visibleIf: function() {return variables["hi"] !== "1"},
+									disabledIf: function() {return variables["hi"] !== "2"},
 
-					name: "d",
+									name: "d",
+								}),
+							]
+						})
+					],
 				}),
 			],
 		}),
